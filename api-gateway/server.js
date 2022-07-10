a6103d61d741f2b1482c8595beafb550e9e6942e
brendonmatos <brendonferreiradm@gmail.com>
2022-07-10 17:25:05 -0300

    feat: gateway proxy services

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -1,9 +1,19 @@
 const express = require('express')
-const app = express()
+const httpProxy = require('express-http-proxy')
 const PORT = process.env.PORT || 3000
+
+const clienteProxy = httpProxy('http://clientes-ws:3000')
+const gerenteProxy = httpProxy('http://gerentes-ws:3000')
+const contaProxy = httpProxy('http://contas-ws:3000')
+
+const app = express()
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.all('/api/v1/clientes', clienteProxy)
+app.all('/api/v1/gerentes', gerenteProxy)
+app.all('/api/v1/contas', contaProxy)
 
 app.listen(PORT, () => {
     console.log('Server started!')
